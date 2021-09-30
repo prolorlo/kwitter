c902aea527021983f01b0c2ff2dfe9d30f4e9eae
Sunhoo <happy0615sh@naver.com>
2021-10-01 02:45:27 +0900

    kwitter 완성

---
+++ src/components/Kweet.js
@@ -0,0 +1,71 @@
+import { dbService, storageService } from "fbase";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faPencilAlt } from "@fortawesome/free-solid-svg-icons";
+
+const Kweet = ({ kweetObj, isOwner}) => {
+    const [editing, setEditing] = useState(false);
+    const [newKweet, setNewKweet] = useState(kweetObj.text);
+    const onDeleteClick = async () => {
+        const ok = window.confirm("Are you sure want to delete this kweet?");
+        if(ok){
+           await dbService.doc(`kweets/${kweetObj.id}`).delete();
+           if(kweetObj.attachmentUrl !== ""){
+            await storageService.refFromURL(kweetObj.attachmentUrl).delete();
+           }
+        }
+    }
+    const toggleEditing = () => setEditing((prev) => !prev);
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await dbService.doc(`kweets/${kweetObj.id}`).update({
+            text: newKweet,
+        });
+        setEditing(false);
+    }
+    const onChange = (event) => {
+        const
+        {target: {value},
+        } = event;
+        setNewKweet(value);
+    }
+    return (
+        <div className="kweet">
+            {editing ? (
+                <>
+            <form onSubmit={onSubmit} className="container kweetEdit">
+                <input
+                type="text"
+                placeholder="Edit your kweet"
+                value={newKweet}
+                required
+                autoFocus
+                onChange={onChange}
+                className="formInput"
+                />
+                <input type="submit" value="Update Kweet" className="formBtn" />
+            </form>
+            <span onClick={toggleEditing} className="formBtn cancelBtn">
+            Cancel
+          </span>
+            </>
+            ) : (<>
+            <h4>{kweetObj.text}</h4>
+            {kweetObj.attachmentUrl && <img src={kweetObj.attachmentUrl} />}
+                {isOwner && (
+                    <div class="kweet__actions">
+                        <span onClick={onDeleteClick}>
+                <FontAwesomeIcon icon={faTrash} />
+              </span>
+              <span onClick={toggleEditing}>
+                <FontAwesomeIcon icon={faPencilAlt} />
+              </span>
+            </div>
+                )}
+                </>
+            )}
+        </div>
+    );
+};
+
+export default Kweet
---
+++ src/routes/Home.js
@@ -0,0 +1,28 @@
+import Kweet from "components/Kweet";
+import { dbService, storageService } from "fbase";
+import React, { useEffect, useState } from "react";
+import KweetFactory from "components/KweetFactory";
+
+const Home = ({userObj}) => {
+    const [kweets, setKweets] = useState([]);
+    useEffect(() => {
+        dbService.collection("kweets").onSnapshot((snapshot) => {
+            const kweetArray = snapshot.docs.map((doc) => ({
+                id:doc.id,
+                ...doc.data(),
+            }));
+            setKweets(kweetArray);
+        });
+    }, []);
+    return (
+        <div className="container">
+            <KweetFactory userObj={userObj}/>
+            <div style={{ marginTop: 30 }}>
+                {kweets.map((kweet) => (
+                <Kweet key={kweet.id} kweetObj={kweet} isOwner={kweet.creatorId === userObj.uid}/>
+                ))}
+            </div>
+        </div>
+    );
+};
+export default Home;
